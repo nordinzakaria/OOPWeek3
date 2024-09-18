f36941fae246ad00dae3135703aeca0ffdbde83e
nordinzakaria <nordinzakaria@gmail.com>
2024-09-18 08:54:08 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // creating an object (variable of a certain class type)
+        Transaction transaction = new Transaction();
+
+        transaction.month = 1;
+        transaction.val = 10.5f;
+
+        Console.WriteLine("transaction value is " + transaction.val);
+    }
+
+}
---
+++ Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class Transaction
+    {
+    public float val;
+    public int month;
+    }
